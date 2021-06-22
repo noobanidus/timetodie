400953aa7a94f2e6d02250278daa2da4b6d41007
Jon McManus <due@wxwhatever.com>
2021-06-22 19:10:02 +1000

    Convert to LivingHurtEvent.

--- src/main/java/noobanidus/mods/timetodie/events/DamageEvent.java
+++ src/main/java/noobanidus/mods/timetodie/events/DamageEvent.java
@@ -2,6 +2,7 @@ package noobanidus.mods.timetodie.events;
 
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import noobanidus.mods.timetodie.TimeToDie;
@@ -10,7 +11,7 @@ import noobanidus.mods.timetodie.init.ModEffects;
 @Mod.EventBusSubscriber(modid=TimeToDie.MODID)
 public class DamageEvent {
   @SubscribeEvent
-  public static void onDamage (LivingDamageEvent event) {
+  public static void onDamage (LivingHurtEvent event) {
     if (event.getEntityLiving().getActivePotionEffect(ModEffects.DYING.get()) != null) {
       if (event.getSource() != DamageSource.OUT_OF_WORLD) {
         event.setCanceled(true);
