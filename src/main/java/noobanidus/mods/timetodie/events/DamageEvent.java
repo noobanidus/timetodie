af0fec8c982fbdad512b370834056d5e13e0f334
Jon McManus <due@wxwhatever.com>
2021-06-24 12:07:58 +1000

    Clean-up, fix reference to incorrect project. Bump version for release.

--- src/main/java/noobanidus/mods/timetodie/events/DamageEvent.java
+++ src/main/java/noobanidus/mods/timetodie/events/DamageEvent.java
@@ -1,17 +1,16 @@
 package noobanidus.mods.timetodie.events;
 
 import net.minecraft.util.DamageSource;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import noobanidus.mods.timetodie.TimeToDie;
 import noobanidus.mods.timetodie.init.ModEffects;
 
-@Mod.EventBusSubscriber(modid=TimeToDie.MODID)
+@Mod.EventBusSubscriber(modid = TimeToDie.MODID)
 public class DamageEvent {
   @SubscribeEvent
-  public static void onDamage (LivingHurtEvent event) {
+  public static void onDamage(LivingHurtEvent event) {
     if (event.getEntityLiving().getActivePotionEffect(ModEffects.DYING.get()) != null) {
       if (event.getSource() != DamageSource.OUT_OF_WORLD) {
         event.setCanceled(true);
